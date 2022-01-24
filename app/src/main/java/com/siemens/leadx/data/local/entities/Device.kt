9408500d11215d7d50d97e8ba6d16718a020229a
norhan.elsawi <norhan.elsawi@ibtikar.net.sa>
2022-01-24 19:56:54 +0200

    test..

--- app/src/main/java/com/siemens/leadx/data/local/entities/Device.kt
+++ app/src/main/java/com/siemens/leadx/data/local/entities/Device.kt
@@ -2,4 +2,7 @@ package com.siemens.leadx.data.local.entities
 
 import com.siemens.leadx.data.remote.entites.LookUp
 
-data class Device(var lookUp: LookUp? = null, var showError: Boolean = false)
+data class Device(
+    var lookUp: LookUp? = null,
+    var showError: Boolean = false,
+)
